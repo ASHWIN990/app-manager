43c1758f44e60edd5a93401f357b13d8b3a762c8
Ashwini Sahu <ashwinisahu990@gmail.com>
2022-10-12 12:45:40 +0530

    Added installation script

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+#Tool = app-manager installer
+#Version = 0.1
+#Author = ASHWINI SAHU
+#GitHub = ASHWIN990
+#Date = 12/10/2022
+#Written in Bash
+
+set -euxo pipefail
+
+function install() {
+
+    if [ "$1" == "file" ]; then
+        echo -e "Installing from the existing file"
+        echo -e "File path : $PWD/app-manager"
+    elif [ "$1" == "internet" ]; then
+        echo -e "Installing from the internet"
+        echo -e "URL : https://raw.githubusercontent.com/ASHWIN990/app-manager/main/app-manager"
+    fi
+
+    if [ ! -d "$HOME/.local/bin/" ]; then
+        mkdir -p "$HOME/.local/bin/"
+    fi
+
+    if [ "$(command -v app-manager)" ]; then
+
+        message=$(echo -e "\napp-manager already installed in $(command -v app-manager)\nInstall it anyway : (y/N) ? : ")
+        read -rp "$message" yn
+
+        case $yn in
+        [Yy]*)
+
+            if [ "$1" == "internet" ]; then
+                echo "Downloading the app-manager" && wget -q "https://raw.githubusercontent.com/ASHWIN990/app-manager/main/app-manager" 
+            fi
+
+            chmod +x "$PWD/app-manager"
+            cp "$PWD/app-manager" "$HOME/.local/bin/"
+            echo -e "\nInstallation done succesfully in $(command -v app-manager)"
+            ;;
+        *)
+            echo -e "Aborting the installation !"
+            exit 0
+            ;;
+        esac
+
+    fi
+}
+
+if [ -e app-manager ]; then
+    install "file"
+else
+    install "internet"
+fi
